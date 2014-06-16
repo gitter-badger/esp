19e871b20046cb0e2a6ec01aae86d2a3d4ebd7f8
Embedthis Software <dev@embedthis.com>
2014-06-16 09:21:55 -0700

    UPGRADE: paks

--- src/paks/appweb/appwebLib.c
+++ src/paks/appweb/appwebLib.c
@@ -2323,17 +2323,16 @@ static int threadStackDirective(MaState *state, cchar *key, cchar *value)
 /*
     Trace options
     Options:
-        connection=NN
+        first=NN
         errors=NN
-        info=NN
-        rxFirst=NN
-        txFirst=NN
-        rxHeader=NN
-        txHeader=NN
-        rxBody=NN
-        txBody=NN
         complete=NN
-        size=NN
+        connection=NN
+        headers=NN
+        context=NN
+        close=NN
+        rx=NN
+        tx=NN
+        content=MAX_SIZE
  */
 static int traceDirective(MaState *state, cchar *key, cchar *value)
 {
@@ -2349,8 +2348,8 @@ static int traceDirective(MaState *state, cchar *key, cchar *value)
         option = stok(option, " =\t,", &ovalue);
         ovalue = strim(ovalue, "\"'", MPR_TRIM_BOTH);
 
-        if (smatch(option, "size")) {
-            httpSetTraceSize(route->trace, (ssize) getnum(ovalue));
+        if (smatch(option, "content")) {
+            httpSetTraceContentSize(route->trace, (ssize) getnum(ovalue));
         } else {
             httpSetTraceEventLevel(route->trace, option, atoi(ovalue));
         }
@@ -5296,7 +5295,7 @@ static bool parseFirstCgiResponse(Cgi *cgi, HttpPacket *packet)
     }
     msg = getCgiToken(buf, "\n");
     mprNop(msg);
-    mprDebug("http cgi", 4, "CGI: Status line: %s %s %s", protocol, status, msg);
+    mprDebug("http cgi", 4, "CGI response status: %s %s %s", protocol, status, msg);
     return 1;
 }
 
